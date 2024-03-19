13c64d8bf28cba38bc09705bc05e1e96c8d8bad2
Hunter Harris <huntervharris@gmail.com>
2024-03-18 18:57:13 -0700

    audio page mobile view

--- src/app/audio/page.tsx
+++ src/app/audio/page.tsx
@@ -1,13 +1,18 @@
+"use client"
+
+import { useIsMobile } from "../../hooks/mobileHooks"
 import "./audio.css"
 import Link from "next/link"
 
 export default function AudioPage(){
 
+    const isMobile = useIsMobile()
+
     return (
         <div>
             <h1>gain audio</h1>
             <a>it all starts here</a>
-            <div className="albums-container">
+            <div className={"albums-container" + (isMobile? "-mobile" : "")}>
                 <Album
                     cover="/art/albums/Commuter cover final.png"
                     title="commuter"
@@ -19,9 +24,11 @@ export default function AudioPage(){
 }
 
 function Album(props: {cover: string, title: string, year: number}){
+    const isMobile = useIsMobile()
+
     const href = `/audio/${props.title}`
     return (
-        <div className="album-item">
+        <div className={"album-item" + (isMobile? "-mobile" : "")}>
             <Link href={href}>
                 <img src={props.cover} style={{maxWidth: "100%"}}/>
             </Link>
