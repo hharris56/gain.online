339188efc1651c62a0bee29f74accbbec435f483
Hunter Harris <huntervharris@gmail.com>
2024-03-19 18:03:59 -0700

    created link back to landing page

--- src/components/header/mobileHeader.tsx
+++ src/components/header/mobileHeader.tsx
@@ -3,6 +3,7 @@
 import "./header.css"
 import { useState } from "react"
 import NavbarMobile from "../navbar/mobileNavbar"
+import Link from "next/link"
 
 export default function MobileHeader(props: any){
     const [isExpanded, setExpanded] = useState(false)
@@ -10,10 +11,10 @@ export default function MobileHeader(props: any){
     return (
         <div className="mobile-header">
             <div className="mobile-header-content">
-                <div style={{display: "flex"}}>
+                <Link href="/landing" className="mobile-header-online">
                     <a>gain.</a>
                     <b>online</b>
-                </div>
+                </Link>
                 <img src="/logos/logo black.png" className="mobile-header-logo"
                 style={{rotate: isExpanded? "45deg" : "0deg"}}
                 onClick={() => setExpanded(!isExpanded)}/>
