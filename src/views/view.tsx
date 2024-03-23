375164aa8a947c29b6466390d31d54bf36689cd9
Hunter Harris <huntervharris@gmail.com>
2024-03-23 14:43:12 -0700

    just useIsMobile()

--- src/views/view.tsx
+++ src/views/view.tsx
@@ -2,47 +2,57 @@
 
 import Header from '../components/header/header'
 import Footer from '../components/footer/footer'
-import { isMobileFn } from '../hooks/mobileHooks'
+import { isMobileFn, useIsMobile } from '../hooks/mobileHooks'
 import MobileView from './mobileView/mobileView'
 import { useEffect, useState } from 'react'
 import { debounce } from 'underscore'
 import { usePathname } from 'next/navigation'
 
-export default function View({
-    children,
-    }: {
-    children: React.ReactNode
-    }){
+export default function View({ children, }: 
+    { children: React.ReactNode }){
 
-    const [layout, setLayout] = useState<any>()
     const pathName = usePathname()
+    // const [layout, setLayout] = useState<any>()
 
-    function handleResize() {
-        setLayout(isMobileFn() ? 
-        // mobile view
-        <MobileView>
+    // function handleResize() {
+    //     setLayout(isMobileFn() ? 
+    //     // mobile view
+    //     <MobileView>
+    //         <div style={{marginBottom: "2rem", fontSize: "1.25em"}}>{children}</div>
+    //     </MobileView> : 
+    //     // desktop view
+    //     <div style={{padding:"2rem"}}>
+    //         <Header />
+    //             <div style={{marginBottom: "2rem", fontSize: "1.25em"}}>{children}</div>
+    //         <Footer />
+    //     </div>
+    //     )
+    // }
+
+    // useEffect(() => {
+    //     handleResize() // call once on initial page load
+    //     var debounced = debounce(handleResize, 100)
+
+    //     // Attach the event listener to the window object
+    //     window.addEventListener('resize', debounced);
+    //     // Remove the event listener when the component unmounts
+    //     return () => {
+    //       window.removeEventListener('resize', debounced);
+    //     };
+    //   }, [])
+
+    var newLayout: any = <></>
+    newLayout = useIsMobile() ? 
+    // mobile view
+    <MobileView>
+        <div style={{marginBottom: "2rem", fontSize: "1.25em"}}>{children}</div>
+    </MobileView> : 
+    // desktop view
+    <div style={{padding:"2rem"}}>
+        <Header />
             <div style={{marginBottom: "2rem", fontSize: "1.25em"}}>{children}</div>
-        </MobileView> : 
-        // desktop view
-        <div style={{padding:"2rem"}}>
-            <Header />
-                <div style={{marginBottom: "2rem", fontSize: "1.25em"}}>{children}</div>
-            <Footer />
-        </div>
-        )
-    }
-
-    useEffect(() => {
-        handleResize() // call once on initial page load
-        var debounced = debounce(handleResize, 100)
-
-        // Attach the event listener to the window object
-        window.addEventListener('resize', debounced);
-        // Remove the event listener when the component unmounts
-        return () => {
-          window.removeEventListener('resize', debounced);
-        };
-      }, [])
+        <Footer />
+    </div>
     
-    return pathName == "/landing" ? <>{children}</> : layout
+    return pathName == "/landing" ? <>{children}</> : newLayout
 }
