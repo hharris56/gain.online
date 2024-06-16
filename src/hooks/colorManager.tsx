82a771737f2a1a5e1dbdc35565a11348461aef27
Hunter Harris <huntervharris@gmail.com>
2024-06-16 13:04:37 -0700

    check for document before setting color

--- src/hooks/colorManager.tsx
+++ src/hooks/colorManager.tsx
@@ -1,8 +1,10 @@
 export default class ColorManager{
     resetColors() {
-        document.documentElement.style.setProperty("--primary-color", "white");
-        document.documentElement.style.setProperty("--accent-color", "lightblue");
-        document.documentElement.style.setProperty("--primary-text-color", "black");
-        document.documentElement.style.setProperty("--secondary-text-color", "black");
+        if (document){
+            document.documentElement.style.setProperty("--primary-color", "white");
+            document.documentElement.style.setProperty("--accent-color", "lightblue");
+            document.documentElement.style.setProperty("--primary-text-color", "black");
+            document.documentElement.style.setProperty("--secondary-text-color", "black");
+        }
     }
 }
