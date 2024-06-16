0dbfe1370b8c9ae7ab4576b56168a97aae5038e6
Hunter Harris <hunterharris@chevron.com>
2024-06-15 22:53:07 -0700

    linkbar changes, added colorManager

---
+++ src/hooks/colorManager.tsx
@@ -0,0 +1,8 @@
+export default class ColorManager{
+    resetColors() {
+        document.documentElement.style.setProperty("--primary-color", "white");
+        document.documentElement.style.setProperty("--accent-color", "lightblue");
+        document.documentElement.style.setProperty("--primary-text-color", "black");
+        document.documentElement.style.setProperty("--secondary-text-color", "black");
+    }
+}
