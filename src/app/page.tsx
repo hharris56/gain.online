f0df2223efa0a7ae44485c5fefabedf47c1270d9
Hunter Harris <huntervharris@gmail.com>
2024-06-25 11:21:10 -0700

    not the best solution but it works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,32 @@
 import "./landing.css";
-import { MobileCoverLogo } from '../components/logo/logo';
+// import { LandingLogo, MobileCoverLogo } from '../components/logo/logo';
 import Link from "next/link";
 
 export default function Landing() {
 
   return (
-    <div className="mobile-cover">
-      {/* <div className="enter-mantle"> */}
-          <Link 
-          className="expand-button enter-button" 
-          href="/home"
-          style={{height: "4rem", width: "40%"}}
-          // passHref={true}
-          >
-          enter
-          </Link>
-      {/* </div> */}
-      <MobileCoverLogo />
+    <div className="landing-page-parent">
+      <Link 
+      className="expand-button enter-button" 
+      href="/home"
+      // style={{height: "4rem", width: "40%"}}
+      // passHref={true}
+      >
+      enter
+      </Link>
+      <LandingLogo />
     </div>
-)
+    )
+}
+
+function LandingLogo(){
+  return (
+      <div>
+          <img src="/logos/logo black.png" className="landing-logo logo-rotate" />
+          <img src="/logos/logo blue.png" className="landing-logo logo-rotate" 
+          style={{animationDelay: "0.2s", zIndex: "-1"}}/>
+          <img src="/logos/logo purple.png" className="landing-logo logo-rotate" 
+          style={{animationDelay: "0.4s", zIndex: "-2"}}/>
+      </div>
+  )
 }
--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -24,4 +24,16 @@ function MobileCoverLogo(){
     )
 }
 
-export { DesktopLogo, MobileCoverLogo }
+function LandingLogo(){
+    return (
+        <div className="landing-logo-platform">
+            <img src="/logos/logo black.png" className="landing-logo logo-rotate" />
+            <img src="/logos/logo blue light.png" className="landing-logo logo-rotate" 
+            style={{animationDelay: "0.2s", zIndex: "-1"}}/>
+            <img src="/logos/logo purple.png" className="landing-logo logo-rotate" 
+            style={{animationDelay: "0.4s", zIndex: "-2"}}/>
+        </div>
+    )
+}
+
+export { DesktopLogo, MobileCoverLogo, LandingLogo }
