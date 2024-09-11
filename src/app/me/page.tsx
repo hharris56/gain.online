b7c2aad5e4f4e0d35182811ea5de8fc7a5200b20
Hunter Harris <huntervharris@gmail.com>
2024-09-10 23:44:27 -0700

    linktree links

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -10,9 +10,9 @@ export default function Me(){
             <h1><i>gain online</i></h1>
             <a>bay area electronic artist</a>
             <a>tech + ambient house / jungle + breakcore</a>
-            <LinkButton icon="spotify" text="spotify" class="linktree-button" sx={{marginTop: "2rem"}} link=""/>
-            <LinkButton icon="apple" text="apple music" class="linktree-button" link=""/>
-            <LinkButton icon="soundcloud" text="soundcloud" class="linktree-button" link=""/>
+            <LinkButton icon="spotify" text="spotify" class="linktree-button" sx={{marginTop: "2rem"}} link="https://open.spotify.com/artist/4mkEgOb3c1imuAXwyeSHug?si=m56OVxS3RmiYkxbTXbws4A"/>
+            <LinkButton icon="apple" text="apple music" class="linktree-button" link="https://music.apple.com/us/artist/gain-online/1744697316"/>
+            <LinkButton icon="soundcloud" text="soundcloud" class="linktree-button" link="https://soundcloud.com/gain_online"/>
         </div>
     )
 }
