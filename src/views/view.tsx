13fd2dca20d48747ff5f1432e0f10530952008bf
Hunter Harris <huntervharris@gmail.com>
2026-01-25 22:10:15 -0800

    wrap the last one

--- src/views/view.tsx
+++ src/views/view.tsx
@@ -1,40 +1,49 @@
-"use client"
+"use client";
 
-import { Header, HeaderBuffer } from '../components/header/header'
-import Footer from '../components/footer/footer'
-import { useIsMobile } from '../hooks/mobileHooks'
-import MobileView from './mobileView/mobileView'
-import { usePathname } from 'next/navigation'
-import ColorManager from '../hooks/colorManager'
+import { useLayoutEffect } from "react";
+import { Header, HeaderBuffer } from "../components/header/header";
+import Footer from "../components/footer/footer";
+import { useIsMobile } from "../hooks/mobileHooks";
+import MobileView from "./mobileView/mobileView";
+import { usePathname } from "next/navigation";
+import ColorManager from "../hooks/colorManager";
 
-export default function View({ children, }: 
-    { children: React.ReactNode }){
+export default function View({ children }: { children: React.ReactNode }) {
+  const pathName = usePathname();
 
-    const pathName = usePathname()
-    const cm = new ColorManager
-    cm.resetColors()
+  useLayoutEffect(() => {
+    const cm = new ColorManager();
+    cm.resetColors();
+  }, []);
 
-    var newLayout: any = <></>
-    newLayout = useIsMobile() ? 
+  var newLayout: any = <></>;
+  newLayout = useIsMobile() ? (
     // mobile view
     <MobileView>
-        <div>{children}</div>
-    </MobileView> : 
+      <div>{children}</div>
+    </MobileView>
+  ) : (
     // desktop view
-    <div style={{padding:"2rem", display: "flex", flexDirection: "row"}}>
-        <Header />
-        <HeaderBuffer />
-        <div style={{display: "flex", flexDirection: "column", padding: "0rem 2rem"}}>
-            <div style={{marginBottom: "2rem", fontSize: "1rem"}}>{children}</div>
-            <Footer />
-        </div>
+    <div style={{ padding: "2rem", display: "flex", flexDirection: "row" }}>
+      <Header />
+      <HeaderBuffer />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "0rem 2rem",
+        }}
+      >
+        <div style={{ marginBottom: "2rem", fontSize: "1rem" }}>{children}</div>
+        <Footer />
+      </div>
     </div>
-    
-    // on landing page, don't show layout
-    return pathName == "/" ? 
-        <>{children}</> 
-        : 
-        <div className="webpage-container">
-            {newLayout}
-        </div>
+  );
+
+  // on landing page, don't show layout
+  return pathName == "/" ? (
+    <>{children}</>
+  ) : (
+    <div className="webpage-container">{newLayout}</div>
+  );
 }
