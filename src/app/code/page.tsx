9352871eba43b8d4d3df5ae34414064db6b56cd7
Hunter Harris <huntervharris@gmail.com>
2026-01-31 17:06:57 -0800

    broken reference

--- src/app/code/page.tsx
+++ src/app/code/page.tsx
@@ -1,5 +1,5 @@
 import "./code.css";
-import { Break } from "../../components/blog/blog";
+import { Break } from "@/features/blog/components";
 import Link from "next/link";
 
 export default function CodePage() {
